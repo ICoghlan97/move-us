afbe6362555cad3b856094166bbcd4b2961f0aca
ICoghlan97 <i.a.coghlan97@gmail.com>
2022-05-03 18:06:39 +0100

    Bug Fixes + Ideas

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,9 +9,9 @@ client = discord.Client()
 
 CHANNEL_ID = 318103513047760899
 HYDRATE_TIME_MINS = 20
-EXERCISE = 30
-PERFORM = ['10 Press-Ups' , '15 Sit-Ups' , '5 Burpees' , '20 Mountain Climbers' , '15 Squats' , '30 Second Plank']
-
+EXERCISE = 0.5
+PERFORM = ['10 Press-Ups' , '15 Sit-Ups' , '5 Burpees' , '20 Mountain Climbers' , '15 Squats' , '30 Second Plank' , '10 Squat Jumps' , '20 Lunges' , '15 Crunches' , '15 Dips' , '10 Leg Raises' , '30 Russian Twists' , '30 Second Side-Plank']
+HYDRATE = ['Time To Hydrate' , 'Drink Some Water' , 'Quench That Thirst' , 'Flush Out That Liver' , 'Wet That Mouth' , 'Give That Throat Some Moisture' , 'Hit The Chug-Jug' , 'Gulp That Liquid' , 'Down That Pint' , 'Sip Some Juice']
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
@@ -30,12 +30,7 @@ async def on_message(message):
 @tasks.loop(minutes=HYDRATE_TIME_MINS)
 async def hydrate_alert():
     channel = client.get_channel(CHANNEL_ID)
-    await channel.send("Time To Hydrate")
-
-@tasks.loop(minutes=EXERCISE)
-async def hydrate_alert():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send(random.choice(PERFORM))
+    await channel.send(random.choice(HYDRATE) + ' 💦 ' + 'and do ' + random.choice(PERFORM) + ' 🏋️‍♀️')
 
 
 client.run(os.getenv("TOKEN"))
